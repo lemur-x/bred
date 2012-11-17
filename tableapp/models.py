da8b7be650ffd6675b52389a8765c6aea8601bb4
student <dro@mail.ru>
2012-11-17 08:02:03 +0100

    create db

--- tableapp/models.py
+++ tableapp/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class schedule(models.Model):
+        id_user = models.IntegerField()
+        date = models.DateField()
+        time = models.IntegerField()
